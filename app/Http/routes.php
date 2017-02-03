7bfdadb869691210db6cf727f9984e5c8575fd3e
Anton Kharhonov <vachovski4@gmail.com>
2017-02-03 22:11:54 +0200

    Adding middleware to pictures route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,19 +1,8 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 use Illuminate\Http\Request;
-Route::get('/',function(){
-    return view('index');
-});
+// Route::get('/',function(){
+//     return view('index');
+// });
 Route::post('/register', [
         'uses' => 'Auth\AuthController@register',
 ]);
@@ -24,9 +13,14 @@ Route::group(['middleware' => 'jwt.auth'], function () {
     Route::get('/user', [
        'uses' => 'UserController@index',
     ]);
+	Route::post('/p/{id}','PictureController@update');
+	Route::post('/p','PictureController@store');
+	Route::delete('/p/{id}','PictureController@destroy');
+
 });
-Route::post('/p/{id}','PictureController@update');
-Route::resource('/p', 'PictureController');
+// Route::resource('/p', 'PictureController');
+Route::get('/p/{id}','PictureController@show');
+Route::get('/p','PictureController@index');
 Route::any('{all}',function(){
 	return view('index');
 })->where(['all'=>'.*']);
